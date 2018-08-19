0daffc3596f70338dcd51054f87f61846d277894
Caroline Berg <carolineeberg@gmail.com>
2018-08-19 20:59:14 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+  attr_accessor :roster, :student_name, :school_name
+def initialize(school_name)
+  @school_name = school_name
+  @roster = Hash.new{ |h, k| h[k] = [] }
+end
+
+  def add_student(student_name, grade_level)
+
+      @roster[grade-level] << student_name
+  end
+
+  def grade(grade_level)
+    @roster[grade_level]
+  end
+
+end
